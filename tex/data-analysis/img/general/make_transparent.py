d2ebed9ab8863d550e649980ce56b5d65e9cc59b
Henry Wilde <henrydavidwilde@gmail.com>
2018-06-08 16:11:14 +0100

    Delete transparency script.

--- tex/data-analysis/img/general/make_transparent.py
+++
@@ -1,9 +0,0 @@
-from glob import iglob
-
-for plot_script in iglob('./**/main.py'):
-    with open(plot_script, 'r') as r:
-        read_file = r.read()
-        new_file = read_file.replace('plt.savefig(filename)',
-                                     'plt.savefig(filename, transparent=True)')
-    with open(plot_script, 'w') as w:
-        w.write(new_file)
