07a23b5fcb4de63c071c91f7ac0bcbbd9c799204
Henry Wilde <henrydavidwilde@gmail.com>
2018-11-07 12:55:26 +0000

    Remove (incomplete) main img gen script.

--- tex/data-analysis/img/main.py
+++
@@ -1,9 +0,0 @@
-import json
-import pandas as pd
-
-# Load in the data
-with open('/Volumes/thesis-data/dtype_dict.json') as f:
-    dtypes = json.load(f)
-
-df = pd.read_csv('/Volumes/thesis-data/main.csv', dtype=dtypes,
-                 parse_dates=['PERIOD', 'ADMDATE', 'DISCDATE'])
