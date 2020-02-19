aedfc22938165cd0f435d0b793a28c75bbf9d9b7
Henry Wilde <henrydavidwilde@gmail.com>
2020-02-19 12:38:48 +0000

    Move data-related scripts to src directory.

---
+++ src/collate_data.py
@@ -0,0 +1,13 @@
+import json
+from glob import iglob
+
+import pandas as pd
+
+with open("dtype_dict.json", "r") as f:
+    dtypes = json.load(f)
+
+csv_files = [csv for csv in iglob("/Volumes/thesis-data/formatted/*.csv")]
+dfs = [pd.read_csv(csv, dtype=dtypes, low_memory=False) for csv in csv_files]
+
+data = pd.concat(dfs, axis=0)
+data.to_csv("/Volumes/thesis-data/main.csv", index=False)
